9af57609c1b71fe8fce0d272a30ec9dcfde28ac7
Jose <josesantillan10@gmail.com>
2025-03-27 14:44:33 -0600

    finish tic-tac-toe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,12 +5,19 @@ import './index.css'
 import { Square } from './components/Square'
 import { WinnerModal } from './components/WinnerModal'
 import { TURNS, WINNER_COMBOS } from './constants'
-import { checkWinner } from './board'
+import { checkWinner } from './logic/board'
+import { saveGameToStorage, resetGameToStorage } from './logic/storage'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   const [winner, setWinner] = useState(null)
 
@@ -22,6 +29,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameToStorage()
   }
 
   const updateBoard = (index) => {
@@ -32,6 +41,11 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
 
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
